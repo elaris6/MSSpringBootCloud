d5ab880febb876d77b496f6bfff44b6cdb6088aa
elaris6 <69580384+elaris6@users.noreply.github.com>
2021-11-14 09:18:29 +0100

    Práctica mejora seguridad JWT hecha de modo chapu

--- 09_MSConSpringSecurityCliente_JWT_Mejorado/src/main/java/com/springboot/msbasico/controller/PersonaController.java
+++ 09_MSConSpringSecurityCliente_JWT_Mejorado/src/main/java/com/springboot/msbasico/controller/PersonaController.java
@@ -11,14 +11,17 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import com.springboot.msbasico.model.Persona;
@@ -34,10 +37,15 @@ public class PersonaController {
 	@Autowired
 	RestTemplate template;
 
-	String url = "http://localhost:8081";
+	/* Se pasan los valores de usuario, password y url del ms final al fichero
+	 * de propiedades. */
+	@Value("${urlmsfinal}")
+	String url;
+	@Value("${jwtuser}")
+	String user;	
+	@Value("${jwtpassword}")
+	String password;
 	
-	String user = "admin";
-	String password = "admin";
 	String token;
 	
 	/* Para incluir la cabecera necesaria con el token, no se pueden usar los
@@ -51,12 +59,27 @@ public class PersonaController {
 	@PostConstruct()
 	public void autenticar() {
 		
-		/* Se llama al método "/login" del servicio en el que nos debemos autenticar
-		 * para conseguir el token JWT que deberá ser usado en el resto de llamadas. */
-		token = template.postForObject(url + "/login?user=" + user + "&password=" + password, null, String.class);
-		// Se añade la cabecera a la lista de cabeceras
-		headers.add("Authorization", "Bearer " + token);
+		System.out.println("--- Inicio método autenticar");
 		
+		/* Se eliminan las cabeceras para que no se dupliquen si ya existen. */
+		headers.remove("Authorization");
+		headers.remove("user");
+		headers.remove("password");
+		
+		/* Se añaden las credenciales en las cabeceras en lugar de como query params. */
+		headers.add("user",user);
+		headers.add("password",password);
+		
+		/* Se llama al método "/login" del servicio en el que nos debemos autenticar
+		 * para conseguir el token JWT que deberá ser usado en el resto de llamadas.
+		 * Al necesitar pasar las credendiales en las cabeceras, se requiere hacer
+		 * la llamada con el método "exchange()" de "RestTemplate". */
+		//token = template.postForObject(url + "/login", null, String.class);
+		ResponseEntity<String> entidadRespuestaLogin = template.exchange(url + "/login", HttpMethod.POST, new HttpEntity<>(headers), String.class);
+		
+		// Se añade la cabecera a la lista de cabeceras
+		headers.add("Authorization", "Bearer " + entidadRespuestaLogin.getBody());
+		System.out.println("--- Fin método autenticar");
 	}
 	
 	@GetMapping(value = "/personas", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -64,9 +87,25 @@ public class PersonaController {
 		
 		/* Se llama al GET de "/agenda/contactos" para recuperar la lista de contactos completa */
 		//Persona[] personas = template.getForObject(url + "/agenda/contactos", Persona[].class);
-		ResponseEntity<Persona[]> entidadRespuesta = template.exchange(url + "/agenda/contactos", HttpMethod.GET, new HttpEntity<>(headers), Persona[].class);
-		Persona[] personas = entidadRespuesta.getBody();
 		
+		ResponseEntity<Persona[]> entidadRespuestaPersonas = null;
+		//https://mflash.dev/blog/2020/07/26/error-handling-for-a-spring-based-rest-api/
+		try {
+			
+			entidadRespuestaPersonas = template.exchange(url + "/contactos", HttpMethod.GET, new HttpEntity<>(headers), Persona[].class);
+			
+		} catch (HttpClientErrorException e) {
+			//System.out.println("--- Status Code: " + entidadRespuestaPersonas.getStatusCodeValue() + " " + entidadRespuestaPersonas.getStatusCode());
+			System.out.println("--- ErrorMessage: " + e.getMessage());
+			if (e.getMessage().startsWith("401")) {
+			//if (entidadRespuesta.getStatusCode().equals(HttpStatus.UNAUTHORIZED)) {
+				System.out.println("--- Recibida respuesta 401. Se solicita renovar token.");
+				autenticar();
+				entidadRespuestaPersonas = template.exchange(url + "/contactos", HttpMethod.GET, new HttpEntity<>(headers), Persona[].class);
+			}
+		}
+
+		Persona[] personas = entidadRespuestaPersonas.getBody();
 		return Arrays.asList(personas);
 		
 	}
@@ -89,12 +128,12 @@ public class PersonaController {
 		// Se hace un POST con la instancia de Persona en el body y sin esperar
 		// respuesta en el body
 		//template.postForLocation(url + "/agenda/contactos", persona);
-		template.exchange(url + "/agenda/contactos", HttpMethod.POST, new HttpEntity<Persona>(persona, headers), String.class);
+		template.exchange(url + "/contactos", HttpMethod.POST, new HttpEntity<Persona>(persona, headers), String.class);
 
 		// Se llama al GET de "/agenda/contactos" para recuperar la lista de contactos
 		// actualizados
 		//Persona[] personas = template.getForObject(url + "/agenda/contactos", Persona[].class);
-		ResponseEntity<Persona[]> entidadRespuesta = template.exchange(url + "/agenda/contactos", HttpMethod.GET, new HttpEntity<>(headers), Persona[].class);
+		ResponseEntity<Persona[]> entidadRespuesta = template.exchange(url + "/contactos", HttpMethod.GET, new HttpEntity<>(headers), Persona[].class);
 		Persona[] personas = entidadRespuesta.getBody();
 
 		// Se transforma el Array de objetos Persona a un objeto de tipo List
@@ -121,7 +160,7 @@ public class PersonaController {
 		// Se llama al GET de "/agenda/contactos" para recuperar la lista de contactos
 		// actualizados
 		//Persona[] personas = template.getForObject(url + "/agenda/contactos", Persona[].class);
-		ResponseEntity<Persona[]> entidadRespuesta = template.exchange(url + "/agenda/contactos", HttpMethod.GET, new HttpEntity<>(headers), Persona[].class);
+		ResponseEntity<Persona[]> entidadRespuesta = template.exchange(url + "/contactos", HttpMethod.GET, new HttpEntity<>(headers), Persona[].class);
 		Persona[] personas = entidadRespuesta.getBody();
 
 		// Se hace un filtro del Array de tipo Persona con la condición deseada y se
